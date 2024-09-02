1c8f354b2e59eafa0d23744ce5a62259f47bbca9
Nymisha <nymisiruvuru99@gmail.com>
2024-09-02 10:50:36 +0530

    complete image upload functonality

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "royal-estate-cf424.firebaseapp.com",
-  projectId: "royal-estate-cf424",
-  storageBucket: "royal-estate-cf424.appspot.com",
-  messagingSenderId: "707158697952",
-  appId: "1:707158697952:web:711d8b56c891f2c2dc6d85"
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
 };
 
 // Initialize Firebase
